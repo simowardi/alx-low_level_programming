141b6faddc6458c4e8129a2e925c9015d7fb28d9
SimoWardi <daoist32@gmail.com>
2023-06-15 11:36:26 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main -  use the function  puts to print string
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
