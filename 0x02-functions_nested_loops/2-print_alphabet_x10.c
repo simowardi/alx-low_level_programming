7210bf67e5db68cda90c72225992e2d242c6679d
SimoWardi <daoist32@gmail.com>
2023-06-20 09:52:07 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+include "main.h"
+
+/**
+ * print_alphabet_x10 - The entry point to a program
+ * that print lowercase letters ten times
+ * using the _putchar function
+ *
+ * Return: always 0(success)
+ */
+
+void print_alphabet_x10(void)
+{
+char ch;
+int i;
+i = 0;
+while (i < 10)
+{
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+i++;
+}
+}
