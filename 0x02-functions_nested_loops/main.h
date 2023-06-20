aa9dea01b7e9fcd081ebb3be5bbc8386802cc4ce
SimoWardi <daoist32@gmail.com>
2023-06-20 10:32:04 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 
 /* Content of main.h */
-int _putchar(char);
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,5 +15,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
+void print_times_table(int n);
 #endif
