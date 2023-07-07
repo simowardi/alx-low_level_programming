3a3ace3af817eb470eadd873b433e1e2e5f7eee4
SimoWardi <daoist32@gmail.com>
2023-07-07 16:11:34 -0700

    libraries are the  future

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int num1, int num2);
+int _isupper(int c);
+int _atoi(char *s);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int _putchar(char c);
+void _puts(char *str);
+
+#endif /* MAIN_H */
