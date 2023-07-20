1d3551a298d10862f76888d16d55cbada0edd2ca
SimoWardi <daoist32@gmail.com>
2023-07-20 13:53:29 -0700

    var fun

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,17 @@
+#include <variadic_functions.h>
+
+/**
+ *main - function that returns the sum of all its parameters
+ *
+ *
+ *Return: Always 0
+ */
+
+int sum_them_all(const unsigned int n, ...);
+{
+	int sum;
+	sum = sum_them_all(n);
+	printf("%d/n" , sum);
+
+	return(0)
+}
