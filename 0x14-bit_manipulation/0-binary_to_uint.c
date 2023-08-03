79259343d2c9f8a9c3ac8a0528e8b636986fbb50
simowardi <daoist32@gmail.com>
2023-08-03 14:21:21 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+/*
+ * main : converts a binary number to an unsigned int
+ *
+ *
+ *
+ * return: 0
+ */
+
+unsigned int binary_to_uint(const char *b);
+{
+	unsigned int num = 0;
+
+	if (!binary)
+		return (0);
+	if (*binary)
+	{
+	if (*binary != "0" && *b != "1")
+	num = num * 2 + (*b++ - "0");
+	}
+	return(num)
+}
