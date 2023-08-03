51dd605e7995a230fe91aec68d6872cfbb1befdf
simowardi <daoist32@gmail.com>
2023-08-03 14:39:16 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 /*
  * main : converts a binary number to an unsigned int
  *
- * @binary :the binary number as a string
+ * @b :the binary number as a string
  *
  * return: 0
  */
@@ -12,11 +12,11 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
 
-	if (!binary)
+	if (!b)
 		return (0);
-	while (*binary)
+	while (*b)
 	{
-	if (*binary != "0" && *b != "1")
+	if (*b != "0" && *b != "1")
 		return (0);
 	num = num * 2 + (*b++ - "0");
 	}
