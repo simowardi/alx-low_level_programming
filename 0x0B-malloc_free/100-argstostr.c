a36b29ab7796cbd6c46df1d1c7d496f188974f8e
simowardi <daoist32@gmail.com>
2023-10-30 19:00:36 -0700

    update

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,76 +1,41 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * _strlen - return the length of a string
- * @s: the string
- * Return: the length
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ * Return: 0
  */
-
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * _strcpy - copies a strings
- * @src: the string to copy
- * @dest: the destination
- * Return: the pointer to dest
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-/**
- * argstostr - concatenates all the arguments
- *	of the program
- * @ac: argument count
- * @av: the arguments
- * Return: pointer to the new string
- */
-
 char *argstostr(int ac, char **av)
 {
-	int i, len_sum = 0;
-	char *str, *total;
+	int i, n, r = 0, l = 0;
+	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
+
 	for (i = 0; i < ac; i++)
 	{
-		_strlen(av[i]);
-		len_sum += _strlen(av[i]) + 1;
+		for (n = 0; av[i][n]; n++)
+			l++;
 	}
-	len_sum++;
+	l += ac;
 
-	str = malloc(len_sum);
+	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
-
-	total = str;
-	for (i = 0; i < ac ; i++)
+	for (i = 0; i < ac; i++)
 	{
-		_strcpy(str, av[i]);
-		str += _strlen(av[i]);
-		*str = '\n';
-		str++;
+		for (n = 0; av[i][n]; n++)
+		{
+			str[r] = av[i][n];
+			r++;
+		}
+		if (str[r] == '\0')
+		{
+			str[r++] = '\n';
+		}
 	}
-	*str = '\0';
-	return (total);
+	return (str);
 }
+
