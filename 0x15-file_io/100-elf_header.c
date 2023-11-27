1f497a6734975a93f5be79e8708a3147e6b91db4
simowardi <daoist32@gmail.com>
2023-11-27 10:23:07 -0800

    update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,103 +1,84 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-char *create_buffer(char *file);
-void close_file(int fd);
+#include "elf_b.h"
+#include "elf_a.h"
 
 /**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
- *
- * Return: A pointer to the newly-allocated buffer.
+ * print_version - Prints the version of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF version.
  */
-char *create_buffer(char *file)
+void print_version(unsigned char *e_ident)
 {
-	char *buffer;
+	printf("  Version:                           %d",
+	       e_ident[EI_VERSION]);
 
-	buffer = malloc(sizeof(char) * 1024);
-
-	if (buffer == NULL)
+	switch (e_ident[EI_VERSION])
+	{
+	case EV_CURRENT:
+		printf(" (current)\n");
+		break;
+	default:
+		printf("\n");
+		break;
+	}
+}
+/**
+ * close_elf - Closes an ELF file.
+ * @elf: The file descriptor of the ELF file.
+ */
+void close_elf(int elf)
+{
+	if (close(elf) == -1)
 	{
 		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
-		exit(99);
-	}
-
-	return (buffer);
-}
-
-/**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
- */
-void close_file(int fd)
-{
-	int c;
-
-	c = close(fd);
-
-	if (c == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
+		"Error: Can't close fd %d\n", elf);
+		exit(98);
 	}
 }
-
 /**
- * main - Copies the contents of a file to another file.
+ * main - Displays the information contained in the
+ *	ELF header at the start of an ELF file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
- *
  * Return: 0 on success.
- *
- * Description: If the argument count is incorrect - exit code 97.
- *              If file_from does not exist or cannot be read - exit code 98.
- *              If file_to cannot be created or written to - exit code 99.
- *              If file_to or file_from cannot be closed - exit code 100.
  */
-int main(int argc, char *argv[])
+int main(__silent int argc, char *argv[])
 {
-	int from, to, r, w;
-	char *buffer;
+	Elf64_Ehdr *h;
+	int O, R;
 
-	if (argc != 3)
+	O = open(argv[1], O_RDONLY);
+	if (O == -1)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	h = malloc(sizeof(Elf64_Ehdr));
+	if (h == NULL)
+	{
+		close_elf(O);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	R = read(O, h, sizeof(Elf64_Ehdr));
+	if (R == -1)
+	{
+		free(h);
+		close_elf(O);
+		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+		exit(98);
 	}
 
-	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	check_elf(h->e_ident);
+	printf("ELF Header:\n");
+	print_magic(h->e_ident);
+	print_class(h->e_ident);
+	print_data(h->e_ident);
+	print_version(h->e_ident);
+	print_osabi(h->e_ident);
+	print_abi(h->e_ident);
+	print_type(h->e_type, h->e_ident);
+	print_entry(h->e_entry, h->e_ident);
 
-	do {
-		if (from == -1 || r == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-		}
-
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
-			free(buffer);
-			exit(99);
-		}
-
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
-
-	} while (r > 0);
-
-	free(buffer);
-	close_file(from);
-	close_file(to);
-
+	free(h);
+	close_elf(O);
 	return (0);
 }
