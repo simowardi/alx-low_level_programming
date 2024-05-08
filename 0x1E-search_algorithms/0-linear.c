52eb3172aa082dba792ba7d5930908c56311c9b7
simowardi <daoist32@gmail@gmail.com>
2024-05-08 06:52:30 -0700

    C search lago task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,14 +4,14 @@
  * linear_search - searches for a value in an array of
  * integers using the Linear search algorithm
  *
- * @array: A pointer to the first element of the array to search.
- * @size: The number of elements in the array.
- * @value: The value to search for.
+ * @array: A pointer to the first element of the array to search .
+ * @size: The number of elements in the array .
+ * @value: The value to search for .
  *
  * Return: If the value is not present or the array is NULL, -1.
- *         else, the first index where the value is located.
+ *         else, the first index where the value is located .
  *
- * Description: Prints a value every time it is compared in the array.
+ * Description: Prints a value every time it is compared in the array .
 **/
 
 
